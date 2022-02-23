7da898607e8799dd4cd8b0fb1e9a1e74fad3d11a
ai-dani <55384817+ai-dani@users.noreply.github.com>
2022-02-23 01:56:27 -0700

    Open journal on current day, cover future days of week

---
+++ Assets/CurrentDayJournal.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentDayJournal : MonoBehaviour
+{
+    Date date;
+    public Button openButton;
+    private GameObject currentDayPanel;
+    private List<GameObject> futureDayPanel;
+    public GameObject coverPanel;
+    private bool OpenOnceAlready = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        futureDayPanel=new List<GameObject>();
+        date = FindObjectOfType<Date>();
+        openButton = FindObjectOfType<Button>();
+        openButton.onClick.AddListener(OpenCurrentDayPanel);
+        openButton.onClick.AddListener(CoverFutureDayPanel);
+        currentDayPanel = date.ReturnCurrentDayTMPText().gameObject.transform.parent.gameObject;
+        foreach(TMPro.TMP_Text futureWeekDay in date.ReturnFutureDayTMPText()){
+            futureDayPanel.Add(futureWeekDay.gameObject.transform.parent.gameObject);
+        }
+    }
+
+    void OpenCurrentDayPanel(){
+        currentDayPanel.SetActive(true);
+    }
+
+    //still working on this extra function - should grey out future date
+    void CoverFutureDayPanel(){
+        if(!OpenOnceAlready){ //avoids multiple instantiations of covers
+            foreach(GameObject futDay in futureDayPanel){
+                GameObject newCover = Instantiate(coverPanel, futDay.transform);
+            }
+            OpenOnceAlready=true;
+        }
+    }
+
+
+    
+}
--- Assets/Date.cs
+++ Assets/Date.cs
@@ -6,11 +6,14 @@ public class Date : MonoBehaviour
 {
     //[SerializeField] TMPro.TMP_Text dateText;
     public TMPro.TMP_Text[] dayOfWeekText; //drag and drop day of the week (7 slots for M-Sun)
+    private TMPro.TMP_Text currentDayText;
+    private List<TMPro.TMP_Text> futureDayText;
 
     void Start()
     {
         // Debug.Log(System.DateTime.Today.AddDays(-1).Date.ToString("MMMM dd, yyyy"));
         // dateText.text = System.DateTime.UtcNow.ToLocalTime().ToString("MMMM dd, yyyy");
+        futureDayText = new List<TMPro.TMP_Text>();
         PopulateDatesForDayOfWeek();
     }
 
@@ -18,10 +21,25 @@ public class Date : MonoBehaviour
     void PopulateDatesForDayOfWeek(){
         int diff = System.DayOfWeek.Monday - System.DateTime.UtcNow.DayOfWeek;
          for(int i = 0; i < dayOfWeekText.Length; i++){
+             if(diff == 0){ //this is the current day of week
+                currentDayText = dayOfWeekText[i];
+             }
+             if(diff > 0){ //future day
+                futureDayText.Add(dayOfWeekText[i]);
+             }
              dayOfWeekText[i].text = System.DateTime.Today.AddDays(diff).Date.ToString("MMMM dd, yyyy");
              diff++;
          }
     }
+
+    public TMPro.TMP_Text ReturnCurrentDayTMPText(){
+        return currentDayText;
+    }
+
+    public List<TMPro.TMP_Text> ReturnFutureDayTMPText(){
+        return futureDayText;
+    }
+
 
 // Original code...
 //THIS WAS EXTREMELY INEFFICIENTLY PAINFULLY HARDCODED AT FIRST... THank goodness for pattern recognition.. and math. :')
