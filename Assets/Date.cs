9b50e6185c9b0bc9b63a3119a4b98259203a8dd4
ai-dani <55384817+ai-dani@users.noreply.github.com>
2022-02-25 11:03:57 -0700

    Update Date.cs

--- Assets/Date.cs
+++ Assets/Date.cs
@@ -8,10 +8,14 @@ public class Date : MonoBehaviour
     public TMPro.TMP_Text[] dayOfWeekText; //drag and drop day of the week (7 slots for M-Sun)
     public TMPro.TMP_Text currentDayText;
     public List<TMPro.TMP_Text> futureDayText;
+    
+    public int currentDayIndex;
+    public List<int> futureDaysIndices;
 
     void Awake()
     {
         futureDayText = new List<TMPro.TMP_Text>();
+        futureDaysIndices = new List<int>();
         PopulateDatesForDayOfWeek();
     }
 
@@ -21,21 +25,35 @@ public class Date : MonoBehaviour
          for(int i = 0; i < dayOfWeekText.Length; i++){
              if(diff == 0){ //this is the current day of week
                 currentDayText = dayOfWeekText[i];
+                currentDayIndex = i;
              }
              if(diff > 0){ //future day
                 futureDayText.Add(dayOfWeekText[i]);
+                futureDaysIndices.Add(i);
              }
              dayOfWeekText[i].text = System.DateTime.Today.AddDays(diff).Date.ToString("MMMM dd, yyyy");
              diff++;
          }
     }
 
+    // Use for journal dates
     public TMPro.TMP_Text ReturnCurrentDayTMPText(){
         return currentDayText;
     }
 
+    // Use for journal dates
     public List<TMPro.TMP_Text> ReturnFutureDayTMPText(){
         return futureDayText;
+    }
+
+    // Use for Mood tracker , water tracker
+    public int CurrentDayIndex(){
+        return currentDayIndex;
+    }
+
+    // Use for Mood tracker , water tracker
+    public List<int> FutureDayIndices(){
+        return futureDaysIndices;
     }
 
 
