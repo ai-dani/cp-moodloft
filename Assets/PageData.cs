cee45efeb7ded0b2106267c6d143a011239ad64d
Aira San Agustin <airadani25@gmail.com>
2022-04-12 16:21:11 -0700

    Journal data entries are loaded on start

--- Assets/PageData.cs
+++ Assets/PageData.cs
@@ -8,16 +8,18 @@ using System.Linq;
 
 public class PageData : MonoBehaviour
 {
-    public string directory = "/Resource/";
-    public string fileName ="journalData.json";
+    public string filePath = "Assets/Prefabs/Data/";
+    public string fileName = "myData.txt";
     public string key;
     public TMPro.TMP_InputField textBox;
     public Button SaveButton;
+    //public Button EditButton;
 
     public void Start(){
         textBox=transform.GetComponentInChildren<TMPro.TMP_InputField>();
-        SaveButton=transform.GetComponentInChildren<Button>();
         SaveButton.onClick.AddListener(SavePage);
+
+        LoadPage();
     }
     
     public void SetKey(string newKey){
@@ -37,11 +39,12 @@ public class PageData : MonoBehaviour
     }
 
     public void SavePage(){
-        string filePath = "Assets/Prefabs/Data/";
-        string fileName = "myData.txt";
+        // string filePath = "Assets/Prefabs/Data/";
+        // string fileName = "myData.txt";
         //string dir = directory;
        // TextAsset jsonFile = dir;
 
+        //if there's actual entry to be saved
         if(!textBox.text.Equals(""))
             if(!Directory.Exists(filePath)){
                 Directory.CreateDirectory(filePath);
@@ -59,19 +62,61 @@ public class PageData : MonoBehaviour
                 // Read existing json file linked in inspector
                 PageCollection pageCol = JsonUtility.FromJson<PageCollection>(reader.ReadToEnd());
                 reader.Close();
-                Debug.Log("previous: " + JsonFile.text);
+
+                //adding a new page to existing page
                 List<Page> listOfPages = new List<Page>();
                 listOfPages = pageCol.pages.ToList();
-                listOfPages.Add(newPage);
+
+                bool keyExists = false;
+                 //if the key from the json file equals to this page's key
+                foreach(Page page in listOfPages){
+                    //if this key already exists, edit existing entry
+                    if(page.key.Equals(GetKey())){ 
+                        page.entry = input;
+                        keyExists=true;
+                        break;
+                    }
+                }
+                //normally add object, if the key doesn't already exist
+                if(!keyExists)
+                    listOfPages.Add(newPage);
+
+                //change to array
                 pageCol.pages = listOfPages.ToArray();
 
                 //save to file
-                string json = JsonUtility.ToJson(pageCol);
+                string json = JsonUtility.ToJson(pageCol, true);
                 File.WriteAllText(filePath+fileName, json);
                 pageCol = JsonUtility.FromJson<PageCollection>(JsonFile.text);
                 Debug.Log("new: " + json);
+
+                //ToggleEdit();
             }  
     }
+
+    public void LoadPage(){
+        // Read existing json file linked in inspector
+        StreamReader reader = new StreamReader(filePath+fileName);
+        PageCollection pageCol = JsonUtility.FromJson<PageCollection>(reader.ReadToEnd());
+        reader.Close();
+        List<Page> listOfPages = new List<Page>();
+        listOfPages = pageCol.pages.ToList();
+
+        //if the key from the json file equals to this page's key
+        foreach(Page page in listOfPages){
+            if(page.key.Equals(GetKey())){
+                if(!page.entry.Equals("")){ //if there's something in the entry
+                    textBox.text=page.entry;
+                    //ToggleEdit();
+                }    
+            }
+        }
+    }
+
+    // public void ToggleEdit(){
+    //     SaveButton.gameObject.SetActive(false);
+    //     EditButton.gameObject.SetActive(true);
+    // }
 
     //REF: https://answers.unity.com/questions/1580286/how-to-append-a-json-array-to-an-already-created-j.html
     public TextAsset JsonFile;
